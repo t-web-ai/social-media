e35e702dbac65d7388897b2ef484d3d4bba3e30a
Toee Thiha Kyaw <toeethiha@cbs.com.mm>
2025-12-10 10:47:12 +0630

    Set up bootstrap.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <div>Application setup.</div>;
+  return (
+    <div className="alert alert-info m-2 fs-5 alert-dismissible">
+      Bootstrap setup.
+      <span className="btn btn-close" data-bs-dismiss="alert"></span>
+    </div>
+  );
 };
 
 export default App;
