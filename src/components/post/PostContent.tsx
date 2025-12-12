83db1c3da7614432917c0dbe3288890096c00b6b
Toee Thiha Kyaw <toeethiha@cbs.com.mm>
2025-12-12 20:22:51 +0630

    Update post content box.

--- src/components/post/PostContent.tsx
+++ src/components/post/PostContent.tsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
 import type { PostResponse } from "../../types/PostResponse";
 
 interface Props {
   post: PostResponse;
 }
 const PostContent = ({ post }: Props) => {
-  const [open, setOpen] = useState(false);
   return (
-    <div
-      className={`fs-5 px-3 p-3 ${
-        open ? "text-wrap text-break" : "text-truncate"
-      } `}
-      onClick={() => setOpen((open) => !open)}
-    >
-      {post.content}
-    </div>
+    <>
+      <div
+        className={`px-3 py-1 text-base`}
+        style={{
+          whiteSpace: "pre-line",
+          wordBreak: "break-all",
+          fontFamily: "system-ui",
+        }}
+      >
+        {post.content}
+      </div>
+      {post.imageUrl && (
+        <div className="p-2 border-top border-2">
+          <img src={post.imageUrl} className="w-100 rounded" />
+        </div>
+      )}
+    </>
   );
 };
 
